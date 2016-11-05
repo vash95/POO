645a0ff70f1847cc4d4644d3227e7d895d945d63
vash <vash@vash-Lenovo-G580>
2016-11-05 14:15:19 +0100

    primer commit

---
+++ uva.poo.entrega1/src/uva/poo/entrega1/TestProducto.java
@@ -0,0 +1,51 @@
+package uva.poo.entrega1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestProducto {
+
+	@Test
+	public void testProductoSinIncializarlo() {
+		Producto p = new Producto ();
+		assertEquals(p.getNombre(),null);
+		assertEquals(p.getUpc(),null);
+		assertEquals(0.0, p.getPrecio(), 0.1);
+	}
+	@Test
+	public void testProductoInicializadoConNombreValido() {
+		Producto p = new Producto ("hola",1.0,"12345678901");
+		assertEquals(p.getNombre(),"hola");
+	}
+	@Test(expected=java.lang.AssertionError.class)
+	public void testProductoInicializadoConNombreInvalido() {
+		Producto p = new Producto (null,-1.0,"12345678901");
+	}
+	@Test
+	public void testProductoInicializadoConPrecio() {
+		Producto p = new Producto ("hola",30.01,"12345678901");
+		assertEquals(30.01, p.getPrecio(), 0.1);;
+	}
+	@Test(expected=java.lang.AssertionError.class)
+	public void testProductoInicializadoConPrecioInvalidoNegativo() {
+		Producto p = new Producto ("hola",-1.0,"12345678901");
+	}
+	@Test(expected=java.lang.AssertionError.class)
+	public void testProductoInicializadoConPrecioInvalidoCero() {
+		Producto p = new Producto ("hola",0.0,"12345678901");
+	}
+	@Test
+	public void testProductoInicializadoConUPCCorrecto() {
+		Producto p = new Producto ("hola",1.0,"12345678901");
+		assertEquals(p.getUpc(),"123456789012");
+	}
+	@Test(expected=java.lang.AssertionError.class)
+	public void testProductoInicializadoConUPCTamaño() {
+		Producto p = new Producto ("hola",1.0,"1234567890");
+	}
+	@Test(expected=java.lang.AssertionError.class)
+	public void testProductoInicializadoConUPCCaracterNoValido() {
+		Producto p = new Producto ("hola",1.0,"1234567890a");
+	}
+}
---
+++ uva.poo.entrega1/src/uva/poo/entrega1/prueba.java
@@ -0,0 +1,30 @@
+package uva.poo.entrega1;
+
+import fabricante.externo.tarjetas.TarjetaMonedero;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		
+		TarjetaMonedero borja = new TarjetaMonedero("A156Bv09_1zXo894",5.0);
+		MaquinaVending 	sen = new MaquinaVending(5,5);
+		Producto polloAlLimon = new Producto("Pollo al limón",5.0,"80808080800");
+		
+		sen.añadirVariosProductos(polloAlLimon, 0, 10);
+		//System.out.println(borja.getSaldoActual());
+		//sen.añadirUnProducto(polloAlLimon,0);
+		//System.out.println(sen.getProducto(0).toString());
+		//System.out.println(sen.getSizeMaq());
+		//System.out.println(sen.getSizeFil(0));
+		//sen.removeOne(0);
+		//System.out.println(sen.getProducto(0).toString());
+		System.out.println(sen.getSizeFil(0));
+		System.out.println("Cuanto cuesta?");
+		System.out.println(sen.precio(0)+" euros");
+		System.out.println("Dame uno");
+		sen.comprar(0, borja);
+		System.out.println(sen.getSizeFil(0));
+		}
+
+}
+	
